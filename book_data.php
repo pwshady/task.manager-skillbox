0950225033f7297d3e56ba2cdab8a723983fe0a6
pwshady <pwshady@gmail.com>
2021-06-29 19:28:59 +0300

    task3-3

--- book_data.php
+++ book_data.php
@@ -1,11 +1,24 @@
 <?php
 
-$authors = ["author1" => ["name" => "FIO1", "email" => "E-MAIL1"], 
-            "author2" => ["name" => "FIO2", "email" => "E-MAIL2"]];
-$books = ["name_book1" => ["name_book" => "BOOK1", "email" => "AUTHOR_E-MAIL1"],
-          "name_book2" => ["name_book" => "BOOK2", "email" => "AUTHOR_E-MAIL2"],
-          "name_book3" => ["name_book" => "BOOK3", "email" => "AUTHOR_E-MAIL3"]];
-$result2 = ["authors" => $authors, "books" => $books];
-var_dump($result2);
+$author = ["EMAIL1" => ["name" => "NAME1", "email" => "EMAIL1", "date" => "01.01.1970"], 
+           "EMAIL2" => ["name" => "NAME2", "email" => "EMAIL2", "date" => "01.01.1980"]];
+$book = [["name_book" => "BOOK1", "email_book" => $author["EMAIL1"]["email"]],
+        ["name_book" => "BOOK2", "email_book" => $author["EMAIL1"]["email"]],
+        ["name_book" => "BOOK3", "email_book" => $author["EMAIL2"]["email"]]];
+
+$result3 = ["authors" => $author, "books" => $book, ];
+var_dump($result3);
+
+for ($i = 0; $i < count($book); $i++)
+{
+    $author_id = $book[$i]["email_book"];
+    print "<p>Book: " . $book[$i]["name_book"] . ". Author: " . $author[$author_id]["name"] . ", born: " . $author[$author_id]["date"] . ", e-mail: " . $author[$author_id]["email"] . ".</p>";
+};
+shuffle($book);
+for ($i = 0; $i < count($book); $i++)
+{
+    $author_id = $book[$i]["email_book"];
+    print "<p>Book: " . $book[$i]["name_book"] . ". Author: " . $author[$author_id]["name"] . ", born: " . $author[$author_id]["date"] . ", e-mail: " . $author[$author_id]["email"] . ".</p>";
+};
 
 ?>
