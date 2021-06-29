1b099b4d050f273a594ddfa7a375ce2b0b84173f
pwshady <pwshady@gmail.com>
2021-06-29 17:51:07 +0300

    task3-2

--- book_data.php
+++ book_data.php
@@ -1,8 +1,11 @@
 <?php
 
-$author = ["name" => "FIO", "email" => "E-MAIL"];
-$book = ["name_book" => "firstBook", "email" => $author["email"]];
-$result1 = [$author, $book];
-var_dump($result1);
+$authors = ["author1" => ["name" => "FIO1", "email" => "E-MAIL1"], 
+            "author2" => ["name" => "FIO2", "email" => "E-MAIL2"]];
+$books = ["name_book1" => ["name_book" => "BOOK1", "email" => "AUTHOR_E-MAIL1"],
+          "name_book2" => ["name_book" => "BOOK2", "email" => "AUTHOR_E-MAIL2"],
+          "name_book3" => ["name_book" => "BOOK3", "email" => "AUTHOR_E-MAIL3"]];
+$result2 = ["authors" => $authors, "books" => $books];
+var_dump($result2);
 
 ?>
