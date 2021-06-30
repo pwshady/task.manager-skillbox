5ac03b818d6433c45a2462e779f4248da5459eea
pwshady <pwshady@gmail.com>
2021-06-30 23:56:34 +0300

    task4-4

---
+++ main_menu.php
@@ -0,0 +1,9 @@
+<?php
+
+$mainMenu = [
+    ["title" => "1", "path" => "/route/1.php", "sort" => 1],
+    ["title" => "2", "path" => "/route/2.php", "sort" => 2],
+    ["title" => "3", "path" => "/route/3.php", "sort" => 3],
+    ["title" => "4", "path" => "/route/4.php", "sort" => 4],
+    ["title" => "5", "path" => "/route/5.php", "sort" => 5],
+];
