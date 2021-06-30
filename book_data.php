3dad4c2804c49df165b37c123969c9e518361be7
pwshady <pwshady@gmail.com>
2021-06-30 23:43:40 +0300

    task4-1

--- book_data.php
+++ book_data.php
@@ -1,10 +1,14 @@
 <?php
 
-$author = ["EMAIL1" => ["name" => "NAME1", "email" => "EMAIL1", "date" => "01.01.1970"], 
-           "EMAIL2" => ["name" => "NAME2", "email" => "EMAIL2", "date" => "01.01.1980"]];
-$book = [["name_book" => "BOOK1", "email_book" => $author["EMAIL1"]["email"]],
-        ["name_book" => "BOOK2", "email_book" => $author["EMAIL1"]["email"]],
-        ["name_book" => "BOOK3", "email_book" => $author["EMAIL2"]["email"]]];
+$author = [
+    "EMAIL1" => ["name" => "NAME1", "email" => "EMAIL1", "date" => "01.01.1970"], 
+    "EMAIL2" => ["name" => "NAME2", "email" => "EMAIL2", "date" => "01.01.1980"],
+];
+$book = [
+    ["name_book" => "BOOK1", "email_book" => $author["EMAIL1"]["email"]],
+    ["name_book" => "BOOK2", "email_book" => $author["EMAIL1"]["email"]],
+    ["name_book" => "BOOK3", "email_book" => $author["EMAIL2"]["email"]],
+];
 
 $result3 = ["authors" => $author, "books" => $book, ];
 //var_dump($result3);
--- index.php
+++ index.php
@@ -29,20 +29,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST")
 	};
 };
 
-
+include($_SERVER["DOCUMENT_ROOT"] . "/layouts/header.php");
 ?>
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link href="styles.css" rel="stylesheet" />
-<title>Project - ведение списков</title>
-</head>
-
-<body>
-
   <div class="header">
     	<div class="logo"><img src="i/logo.png" width="68" height="23" alt="Project" /></div>
         <div style="clear: both"></div>
@@ -91,8 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST")
     
     <div class="footer">&copy;&nbsp;<nobr>2018</nobr> Project.</div>
 
-</body>
-</html>
-<?php
 
+<?php
+include($_SERVER["DOCUMENT_ROOT"] . "/layouts/headerfooter.php");
 print ($_SERVER['REQUEST_METHOD']);
